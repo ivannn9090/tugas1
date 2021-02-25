5d29df9017cf1b58c6535a54306ee0bb39573ae4
ivannn9090 <64771719+ivannn9090@users.noreply.github.com>
2021-02-25 09:42:41 -0800

    tugas1

---
+++ myweb.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <title>Sistem Informasi Seputar Macbook</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/myweb.css" type="text/css">
+    <link rel="stylesheet" href="assets/navbar.css" type="text/css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <h1> JUDUL </h1>
+            <h2>SUBJUDUL</h2>
+           
+        </div>
+        <ul>
+              <li><a class="active" href="#link">link</a></li>
+              <li><a href="#link">link</a></li>
+              <li><a href="#link">linkt</a></li>
+           </ul>
+        <div class="row">
+             <div class="left">
+             <h3> side bar</h3>
+             
+                
+             </div>
+             <div class="right">
+             
+                <h3><strong> CONTENT</strong></h3>
+                <strong> pada bagian content adalah tempat berita utama akan di perlihatkan dan juga akan memperlihatakan gambar maupun video di tempat ini juga adalah tempat dimana seorang yang membaut konten akan mudah di baca dan di perlihatkan kepada orang lain</strong>
+             </div>   
+        </div>
+        <div style="clear:both"></div>
+        <div class="row">
+             <div class="footer">
+                 <div id="text">
+                     footer
+                 </div>
+             </div>
+        </div>
+ </div>
+</body>
+
+</html>
