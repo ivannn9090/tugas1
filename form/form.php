c9055c632a08e4be42eff69a25f5a7eef119af10
ivannn9090 <64771719+ivannn9090@users.noreply.github.com>
2021-03-10 12:23:03 +0800

    Update form.php

--- form/form.php
+++ form/form.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
      <head>
-         <title>Sistem Informasi Seputar Macbook</title>
+         <title>HUBUNGI KAMI</title>
          <meta charset="utf-8">
          <meta name="viewport" content="width=device-width, initial-scale=1">
          <link rel="stylesheet" href="assets/myweb.css" type="text/css">
-         <link rel="stylesheet" href="assets/navbar.css" type="text/css">
-
-         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+       
+                        
+         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+       
+         
      </head>
      <body>
          <div class="wrapper">
@@ -17,11 +18,37 @@
                  <h1> JUDUL </h1>
                  <h2>SUBJUDUL</h2>
             </div>
-            <ul>
-              <li><a class="active" href="http://localhost/tugas1/myweb.php">HOME</a></li>
-              <li><a href="http://localhost/tugas1/form/form.php">hubungi kami</a></li>
-              <li><a href="#link">linkt</a></li>
-           </ul>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="http://localhost/tugas1/myweb.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="http://localhost/tugas1/form/form.php">Hubungi Kami</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Tugas
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="http://localhost/tugas1/math/fibonacci.php">Fibonacci</a></li>
+            <li><a class="dropdown-item" href="http://localhost/tugas1/math/arematika.php">Arimatika</a></li>
+           
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
            <div class="right"> 
            <div class="col-lg-8 " >
